c9ad114e38eeae5d64c90c832c57df26dc308609
BuraYu <burak.yueksel@hotmail.com>
2024-03-22 17:04:58 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Team from "./scenes/team";
 // import Invoice from "./scenes/dashboard";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
+import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/dashboard";
 // import FAQ from "./scenes/faq";
@@ -31,9 +31,9 @@ function App() {
               <Route path="/team" element={<Team />} />
               {/* <Route path="/invoice" element={<Invoice />} />
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/bar" element={<Bar />} />
+              <Route path="/bar" element={<Bar />} /> */}
               <Route path="/form" element={<Form />} />
-              <Route path="/line" element={<Line />} />
+              {/* <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
